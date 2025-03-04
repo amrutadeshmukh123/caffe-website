f1520f14ac4129a3a4f10d1f64a6a4db60192a8c
Amruta Deshmukh <deshmukhamruta394@gmail.com>
2025-03-04 18:09:44 +0530

    update

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react';
 import './Header.css'
 
-export default function Header(){
-    return(
-        <>
-           <div className="header">
-              <h2>Dreamland Cafe</h2>
-              <div className="links">
-                <a href="#">Home</a>
-                <a href="#">About us</a>
-                <a href="#">Offers</a>
-                <a href="#">Services</a>
-                <button>Know more!</button>
-              </div>
-           </div>
-        </>
-    );
+export default function Header() {
+
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+
+
+  return (
+
+  <>
+    <div className="header">
+      <h2>Dreamland Cafe</h2>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <i class="fa-solid fa-xmark"></i> : <i class="fa-solid fa-bars"></i>}
+      </div>
+
+      <div className="links">
+        <a href="#" onClick={toggleMenu}>Home</a>
+        <a href="#" onClick={toggleMenu}>About us</a>
+        <a href="#" onClick={toggleMenu}>Offers</a>
+        <a href="#" onClick={toggleMenu}>Services</a>
+        <button onClick={toggleMenu}>Know more!</button>
+      </div>
+    </div>
+  </>
+  );
 }
